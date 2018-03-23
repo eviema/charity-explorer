11d9cb9d1c9971989468186bc7cffa3fb4821e21
Evie Ma <yfma89@gmail.com>
2018-03-23 12:02:09 +1100

    config to run express and react concurrently in dev env

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const visualRoute = require('./routes/visualRoute');
+const charityRoute = require('./routes/charityRoute');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+visualRoute(app);
+charityRoute(app);
 
 app.get('/', (req, res) => {
     res.send({ hi: 'there' });
