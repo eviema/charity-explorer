e362ce0a5cd85159df898e437cfc85097e5af207
Evie Ma <yfma89@gmail.com>
2018-03-29 13:20:25 +1100

    make sure react is used for front-end in production env

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const visualRoute = require('./routes/visualRoute');
-const charityRoute = require('./routes/charityRoute');
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-visualRoute(app);
-charityRoute(app);
+require('./models/Charity');
+require('./models/Cause');
+require('./models/Location');
+require('./routes/charityRoute')(app);
+require('./routes/visualRoute')(app);
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets, 
+    // e.g. main.js, main.css 
+    app.use(express.static('client/build'));
+
+    // Express will serve up index.html
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
