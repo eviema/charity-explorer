94bcb7b352f0a257d351a3549f46c34b4206ece0
Evie Ma <yfma89@gmail.com>
2018-03-30 21:49:00 +1100

    added breadcrumbs to About Us page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'mdbreact';
 
 const About = () => {
     return(
         <div>
+            <Breadcrumb>
+                <BreadcrumbItem><a href="/home"><i class="fa fa-home fa-lg"></i></a></BreadcrumbItem>
+                <BreadcrumbItem active>About us</BreadcrumbItem>
+            </Breadcrumb>
             this is about us page. 
         </div>
     );
