0b0a852ebfaf4f3fb5625efa58bafeec7d122f84
Evie Ma <yfma89@gmail.com>
2018-03-30 21:41:57 +1100

    added Footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Col, Container, Row } from "mdbreact";
+
+const Footer = () => {
+    return(
+        <footer className="page-footer stylish-color-dark font-small">
+            <Container className="py-5">
+            <Row className="text-center d-flex justify-content-center">
+                <Col md="2">
+                    <h6 className="title font-weight-bold"><a href="/home">Home</a></h6>
+                </Col>
+                <Col md="2">
+                    <h6 className="title font-weight-bold"><a href="/charities/dashboardAct">Charitable causes</a></h6>
+                </Col>
+                <Col md="2">
+                    <h6 className="title font-weight-bold"><a href="/charitySearch">Charities</a></h6>
+                </Col>
+                <Col md="2">
+                    <h6 className="title font-weight-bold"><a href="/about">About</a></h6>
+                </Col>
+                <Col md="2">
+                    <h6 className="title font-weight-bold"><a href="/contact">Contact</a></h6>
+                </Col>
+            </Row>
+            </Container>
+            <div className="footer-copyright text-center py-4">
+                <Container fluid>
+                    &copy; {(new Date().getFullYear())} DonateNow
+                    <br />
+                    Powered by ACE Solutions
+                </Container>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
