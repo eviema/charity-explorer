0a3474697ecaf87c023c7c5c40b569b59c30a87a
Evie Ma <yfma89@gmail.com>
2018-03-30 21:49:44 +1100

    added breadcrumbs to Contact us page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'mdbreact';
 
 const Contact = () => {
     return(
         <div>
+            <Breadcrumb>
+                <BreadcrumbItem><a href="/home"><i class="fa fa-home fa-lg"></i></a></BreadcrumbItem>
+                <BreadcrumbItem active>Contact us</BreadcrumbItem>
+            </Breadcrumb>
             this is contact us page. 
         </div>
     );
