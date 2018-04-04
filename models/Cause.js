a25435097624f81c8a6c375bc488d693fea97372
Evie Ma <yfma89@gmail.com>
2018-04-04 16:10:43 +1000

    updated schema for Cause

--- models/Cause.js
+++ models/Cause.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const causeSchema = new Schema({
-    Main_Activity: String
+    Main_Activity: String,
+    Subtype_Name: String,
+    Subtype_Desc: String,
+    Example: String
 });
 
 module.exports = mongoose.model('causes', causeSchema);
