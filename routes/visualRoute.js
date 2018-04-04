a59653d5416ec70790e304a0ff51f5edb621f3ab
Evie Ma <yfma89@gmail.com>
2018-04-04 16:11:47 +1000

    deleted unused route file

--- routes/visualRoute.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-    app.get(
-        '/api/charities-visual',
-        (req, res) => {
-            res.send('This is the page that shows visualiations and so on...');
-        }
-    );
-};
