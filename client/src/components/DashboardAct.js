af8bbfd8c9788b5b76c7cd1a18372fa4d7f157ad
Evie Ma <yfma89@gmail.com>
2018-04-05 23:36:23 +1000

    rewrote explore causes page

--- client/src/components/DashboardAct.js
+++ client/src/components/DashboardAct.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router";
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImage, CardTitle, CardText } from "mdbreact";
+import { Redirect } from 'react-router';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImage } from "mdbreact";
 import axios from 'axios';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label, ResponsiveContainer } from 'recharts';
+import spinner from '../assets/spinner.gif';
+const greaterMelb = require("./greaterMelb");
 
 class DashboardAct extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      redirectToSearch: false,
+      searchCharityClicked: false,
       locationsAll: [],
       locationCurrent: '',
       causesByLocation: [],
-      causeCurrent: '',
+      causeCurrentDetails: [],
+      causeName: '',
+      causeCharityCount: 0,
+      causeDonations: 0,
+      causeGrants: 0,
+      barClicked: false,
+      loading: false,
     };
     this.handleOnClickToSearch = this.handleOnClickToSearch.bind(this);
     this.handleInputChangeOfLocation = this.handleInputChangeOfLocation.bind(this);
+    this.handleClickOnCauseBar = this.handleClickOnCauseBar.bind(this);
   }
 
   componentDidMount() {
@@ -35,20 +44,34 @@ class DashboardAct extends Component {
         .catch(function(e) {
             console.log("ERROR", e);
         });
+    
+    this.setState({
+        locationCurrent: 'Greater Melbourne',
+        causesByLocation: greaterMelb
+    });
 
   }
 
   async handleInputChangeOfLocation(event) {
+
     await this.setState({ 
-        locationCurrent: event.target.value
+        locationCurrent: event.target.value,
+        barClicked: false,
+        loading: true,
     });
 
-    console.log(this.state.locationCurrent);
-
-    axios.get('/api/charitiesByLoc/' + this.state.locationCurrent)
+    if (this.state.locationCurrent === 'Greater Melbourne') {
+        this.setState({
+            causesByLocation: greaterMelb,
+            loading: false,
+        });
+    }
+    else {
+        axios.get('/api/charitiesByLoc/' + this.state.locationCurrent)
         .then((res) => {
+
             var causesByLocMatched = [];
-            console.log(res.data);
+
             res.data.forEach((entry) => {
                 var currentCause = entry['Main_Activity'].trim(),
                     currentAmtDonations = entry['Donations_and_bequests'],
@@ -80,28 +103,67 @@ class DashboardAct extends Component {
 
             this.setState({
                 causesByLocation: causesByLocMatched,
+                loading: false,
             });
 
-            console.log(this.state.causesByLocation);
+            // console.log(this.state.causesByLocation);
 
         })
         .catch(function(e) {
             console.log("ERROR", e);
         });
+    }
 
   }
 
-  handleOnClickToSearch = () => {
+  handleClickOnCauseBar(data) {
+    
+    var causeClickedName = data.name;
+
+    axios.get('/api/causes/' + causeClickedName)
+        .then((res) => {
+
+            this.setState({
+                causeCurrentDetails: res.data,
+            });
+
+        })
+        .catch(function(e) {
+            console.log("ERROR", e);
+        }); 
+    
+    var causeIndex = this.state.causesByLocation.findIndex((cause => cause.name === causeClickedName));
+    
     this.setState({
-      redirectToSearch: true
+        barClicked: true,
+        causeName: causeClickedName,
+        causeCharityCount: this.state.causesByLocation[causeIndex].charityCount,
+        causeDonations: this.state.causesByLocation[causeIndex].amtDonations,
+        causeGrants: this.state.causesByLocation[causeIndex].amtGrants,
+    }); 
+    
+  }
+
+  handleOnClickToSearch() {
+    this.setState({
+        searchCharityClicked: true
     });
-  };
+  }
 
   render() {
-    if (this.state.redirectToSearch) {
-      return <Redirect push to="/charitySearch" />;
-    }
 
+    if (this.state.searchCharityClicked) {
+        return (
+            <Redirect to={{
+                pathname: '/charitySearch',
+                state: {
+                    cause: this.state.causeName,
+                    location: this.state.locationCurrent
+                }
+            }}/>
+        );
+    }
+    
     const data = this.state.causesByLocation;
 
     return (
@@ -124,47 +186,34 @@ class DashboardAct extends Component {
                     {this.state.locationCurrent !== '' && <span>{this.state.locationCurrent}</span>}
                 </strong>
             </p> 
-            <select className="col-8 col-sm-6 col-md-3 col-lg-2 small py-1"
-                    name="location"
-                    placeholder="Select suburb..."
-                    value={this.state.locationCurrent}
-                    onChange={this.handleInputChangeOfLocation}>
-                {this.state.locationsAll.map(location => <option key={location} value={location}>{location}</option>)}
-            </select>
+            <div className="col-1">
+                {this.state.loading && 
+                    <img src={spinner} alt="loading..." style={{height: 30, paddingLeft:30}}/>
+                }
+            </div>
+            <div className="col-10 col-sm-10 col-md-8 col-lg-5 col-xl-3 small py-1 px-1">
+                <div className="row d-flex align-items-center justify-content-center">
+                    <span className="mr-2">Change location: </span>
+                    <select 
+                        name="location"
+                        placeholder="Select suburb..."
+                        value={this.state.locationCurrent}
+                        onChange={this.handleInputChangeOfLocation}>
+                        {this.state.locationsAll.map(location => <option key={location} value={location}>{location}</option>)}
+                    </select>
+                </div> 
+                
+            </div>
+                        
         </div>
         
         <hr />
 
         {/* info + graph */}
-        <div className="row d-flex align-items-center justify-content-center py-1 mx-2">
-
-            {/* details info of cause in location */}
-            <div className="col-11 col-sm-11 col-md-11 col-lg-3 col-xl-3">
-                { 
-                    this.state.causeCurrent === '' &&
-                    <p>Click on a cause from the graph to see detailed information here.</p>
-                }
-                { 
-                    this.state.causeCurrent !== '' &&
-                    <Card cascade>
-                        <CardImage tag="div">
-                            <div className="#26c6da cyan lighten-1 p-4">
-                                <h5 className="h5-responsive">{this.state.causeCurrent} in {this.state.location}</h5>
-                            </div>
-                        </CardImage>
-                        <CardBody>
-                            <CardTitle>{this.state.causeCurrent}</CardTitle>
-                            <CardText>
-                                                           
-                            </CardText>
-                        </CardBody>
-                    </Card>
-                }
-                
-            </div>
+        <div className="row d-flex align-items-stretch justify-content-center py-1 mx-1 mb-4">
 
             {/* graph of causes in location */}
-            <div className="col-11 col-sm-11 col-md-11 col-lg-8 col-xl-8 small" style={{height:"90vh"}}>
+            <div className="col-11 col-sm-11 col-md-11 col-lg-6 col-xl-6 small" style={{height:"90vh"}}>
                 <ResponsiveContainer>
                     <BarChart 
                             data={data} layout="vertical"
@@ -178,12 +227,68 @@ class DashboardAct extends Component {
                         <CartesianGrid strokeDasharray="3 3"/>
                         <Tooltip/>
                         <Legend verticalAlign="top" align="right" offset={10} iconType="star"/>
-                        <Bar dataKey="amtDonations" name="Donations and bequests (AUD)" stackId="a" fill="#8884d8">
-                        </Bar>
-                        <Bar dataKey="amtGrants" name="Government grants (AUD)" stackId="a" fill="#82ca9d" />
+                        <Bar dataKey="amtDonations" name="Donations and bequests (AUD)" stackId="a" fill="#8884d8" onClick={data=>this.handleClickOnCauseBar(data)} />
+                        <Bar dataKey="amtGrants" name="Government grants (AUD)" stackId="a" fill="#82ca9d" onClick={data=>this.handleClickOnCauseBar(data)} />
                     </BarChart>
                 </ResponsiveContainer>
             
+            </div>
+
+            {/* details info of cause in location */}
+            <div className="col-11 col-sm-11 col-md-11 col-lg-5 col-xl-5 mt-3">
+                { 
+                    !this.state.barClicked &&
+                    <p>Click on a bar from the graph to see detailed information of a cause here.</p>
+                }
+                { 
+                    this.state.barClicked &&
+                    <div id="causeInfo" >
+                        <Card cascade className="mt-2 mb-4">
+                            <CardImage tag="div">
+                                <div className="#81d4fa light-blue lighten-3 p-4">
+                                    <h5 className="h5-responsive">{this.state.causeName}</h5><br />
+                                    <h6 className="h6-responsive">{this.state.locationCurrent}</h6>
+                                </div>
+                            </CardImage>
+                            <CardBody style={{color:"#616161", fontSize:"small"}}>
+                                <p>
+                                    In 2016, charities supporting {this.state.causeName} in {this.state.locationCurrent} received <strong>${this.state.causeDonations} donations and bequests</strong> and <strong>${this.state.causeGrants} government grants</strong>.
+                                </p>
+                                <p>
+                                    There are <strong>{this.state.causeCharityCount} charities</strong> supporting {this.state.causeName} in {this.state.locationCurrent}.
+                                </p>
+                                <button className="btn btn-info" type="button" onClick={this.handleOnClickToSearch}>See complete charity list</button>
+                            </CardBody>
+                        </Card>
+
+                        <Card cascade>
+                            <CardImage tag="div">
+                                <div className="#80deea cyan lighten-3 p-4">
+                                    <h5 className="h5-responsive">What is "{this.state.causeName}"</h5>
+                                </div>
+                            </CardImage>
+                            <CardBody>
+                                <p className="h6-responsive">
+                                        {this.state.causeCurrentDetails.length}&nbsp;
+                                        {this.state.causeCurrentDetails.length === 1 && <span>subcategory </span>}
+                                        {this.state.causeCurrentDetails.length !== 1 && <span>subcategories </span>}
+                                    of work
+                                </p>
+                                {this.state.causeCurrentDetails.map(subtype => 
+                                        <div key={subtype["_id"]} style={{color:"#616161", fontSize:"small"}}>
+                                            <hr />
+                                            <p className="h5-responsive font-weight-bold">{subtype["Subtype_Name"]}</p>
+                                            <p>{subtype["Subtype_Desc"]}</p>
+                                            <p className="font-italic"><strong>Example: </strong>{subtype["Example"]}</p>
+                                        </div>
+                                    )
+                                }
+                            </CardBody>
+                        </Card>
+                    </div>
+                    
+                }
+                
             </div>
         </div>
         
