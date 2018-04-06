bf83d471d1eb2fd4e09d7371f5741297117265aa
Evie Ma <yfma89@gmail.com>
2018-04-06 23:13:27 +1000

    rearranged display of charity search results

--- client/src/components/CharitySearch.js
+++ client/src/components/CharitySearch.js
@@ -3,8 +3,11 @@ import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 import axios from 'axios';
 import spinner from '../assets/spinner.gif';
-import { Card, CardBody, CardImage, CardTitle, CardText, Breadcrumb, BreadcrumbItem, Container, Col, Row } from 'mdbreact';
+import { Card, CardBody, CardImage, CardTitle, CardText, 
+        Breadcrumb, BreadcrumbItem, 
+        Container, Col, Row } from 'mdbreact';
 import searchBackground from '../assets/searchBackground.jpg';
+import Pagination from "react-js-pagination";
 
 class CharitySearch extends Component {
     constructor(props) {
@@ -29,6 +32,8 @@ class CharitySearch extends Component {
             causes: [],
             locations: [],
             charities: [],
+            currentPage: 1,
+            charitiesPerPage: 6,
             doneCharitySearch: false,
             loading: false,
             isMobileDevice: false,
@@ -36,6 +41,8 @@ class CharitySearch extends Component {
         this.handleInputChangeOfCause = this.handleInputChangeOfCause.bind(this);
         this.handleInputChangeOfLocation = this.handleInputChangeOfLocation.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClickOnPageNumber = this.handleClickOnPageNumber.bind(this);
+        this.handleClickToSearch = this.handleClickToSearch.bind(this);
     }
 
     componentDidMount() {
@@ -117,11 +124,11 @@ class CharitySearch extends Component {
         });
     }
 
-
     handleSubmit(event) {
 
         this.setState({
-            loading: true
+            loading: true,
+            doneCharitySearch: false,
         });
 
         axios.get('/api/charities/' 
@@ -156,6 +163,19 @@ class CharitySearch extends Component {
         event.preventDefault();
     }
 
+    handleClickOnPageNumber(currentPageNumber) {        
+        this.setState({
+            currentPage: currentPageNumber
+        });
+        window.scrollTo(0, 0);
+    }
+
+    handleClickToSearch() {
+        this.setState({
+            doneCharitySearch: false
+        });
+    }
+
     render() {
         
         var { cause } = this.state;
@@ -164,30 +184,43 @@ class CharitySearch extends Component {
         var { location } = this.state;
         var valueLocation = location && location.value;
 
-        var charityListItems = this.state.charities.map((charity, index) =>          
-            <li key={index} className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 d-flex align-items-stretch p-3">
+        const { charities, currentPage, charitiesPerPage } = this.state;
 
-                <Card cascade>
-                    <CardImage tag="div">
-                        <div className="#26c6da cyan lighten-1 p-4">
-                            <h5 className="h4-responsive">{charity.name}</h5>
-                        </div>
-                    </CardImage>
-                    <CardBody>
-                        <CardTitle>{charity.cause}</CardTitle>
-                        <CardText>
-                            {charity.desc.length <= 200 ? charity.desc : charity.desc.slice(0,200).concat("... ")}
-                            <br />
-                            {/* <a className="btn btn-default" href={`/charity/${charity.ABN}`} >
-                                Learn more 
-                                <i class="fa fa-angle-double-right pl-2"></i>
-                            </a> */}                            
-                        </CardText>
-                    </CardBody>
-                </Card>
+        const indexOfLastCharity = currentPage * charitiesPerPage;
+        const indexOfFirstCharity = indexOfLastCharity - charitiesPerPage;
+        const currentCharities = charities.slice(indexOfFirstCharity, indexOfLastCharity);
 
-            </li>
-        );
+        const renderCharities = currentCharities.map((charity, index) => {
+            return (
+                <li key={index} className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 d-flex align-items-stretch p-3">
+
+                    <Card cascade>
+                        <CardImage tag="div">
+                            <div className="#26c6da cyan lighten-1 p-4">
+                                <h5 className="h4-responsive">{charity.name}</h5>
+                            </div>
+                        </CardImage>
+                        <CardBody>
+                            <CardTitle>{charity.cause}</CardTitle>
+                            <CardText>
+                                {charity.desc.length <= 200 ? charity.desc : charity.desc.slice(0,200).concat("... ")}
+                                <br />
+                                {/* <a className="btn btn-default" href={`/charity/${charity.ABN}`} >
+                                    Learn more 
+                                    <i class="fa fa-angle-double-right pl-2"></i>
+                                </a> */}                            
+                            </CardText>
+                        </CardBody>
+                    </Card>
+
+                </li>
+            );
+        });
+
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(charities.length * 1.0 / charitiesPerPage); i++) {
+            pageNumbers.push(i);
+        }
 
         var pageStyle = this.state.isMobileDevice 
             ? {
@@ -196,96 +229,143 @@ class CharitySearch extends Component {
                 backgroundAttachment: "scroll",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
-                height: "80vh",
+                height: "90vh",
             } : {
                 background: `url(${searchBackground})`,
                 backgroundRepeat: "no-repeat",
                 backgroundAttachment: "fixed",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
-                height: "80vh",
+                height: "90vh",
             }
-
-        var searchBoxStyle = {
+        
+         var searchBoxStyle = {
             background: "rgba(236, 239, 241, 0.85)",
             padding: "2rem",
             margin: "2rem",
         }
 
         return(
-            <div style={pageStyle}>
-                <Breadcrumb className="small">
+            <div>
+                <Breadcrumb className="small mb-0">
                     <BreadcrumbItem><a href="/home"><i className="fa fa-home"></i></a></BreadcrumbItem>
                     <BreadcrumbItem><a href="/charities/dashboardAct">Explore charitable causes</a></BreadcrumbItem>
                     <BreadcrumbItem active>Search for charities</BreadcrumbItem>
                 </Breadcrumb>
-                <form className="pt-3 mx-2" > 
-                    <div className="my-5"></div>
-                    <div style={searchBoxStyle} className="m-3">
-                        <div className="row">
-                            <div className="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-6">
-                                <h5 className="mb-3 h5">Which charitable cause has personal significance to you? </h5>         
-                                <Select name="cause"
-                                    placeholder="Enter your charitable cause of interest..."
-                                    value={valueCause}
-                                    onChange={this.handleInputChangeOfCause}
-                                    options={this.state.causes} />
-                                <a href="/charities/dashboardAct"
-                                    className="my-2 w-50 small" style={{textShadow: "1px 1px 8px #fff"}}>
-                                    Still unsure about which cause to choose? Click here to continue exploring...
-                                </a> 
 
-                                <h5 className="my-3 h5">And your location?</h5>
-                                <Select name="location"
-                                    placeholder="Enter your location..."
-                                    value={valueLocation}
-                                    onChange={this.handleInputChangeOfLocation}
-                                    options={this.state.locations} />
+                {
+                    (!this.state.doneCharitySearch || this.state.charities.length === 0) &&
+                
+                    <form className="pt-3 px-2" style={pageStyle}> 
+                        <div style={searchBoxStyle} className="m-3">
+                            <div className="row">
+                                <div className="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-6">
+                                    <h5 className="mb-3 h5">Which charitable cause has personal significance to you? </h5>         
+                                    <Select name="cause"
+                                        placeholder="Enter your charitable cause of interest..."
+                                        value={valueCause}
+                                        onChange={this.handleInputChangeOfCause}
+                                        options={this.state.causes} />
+                                    <a href="/charities/dashboardAct"
+                                        className="my-2 w-50 small" style={{textShadow: "1px 1px 8px #fff"}}>
+                                        Still unsure about which cause to choose? Click here to continue exploring...
+                                    </a> 
+
+                                    <h5 className="my-3 h5">And your location?</h5>
+                                    <Select name="location"
+                                        placeholder="Enter your location..."
+                                        value={valueLocation}
+                                        onChange={this.handleInputChangeOfLocation}
+                                        options={this.state.locations} />
+                                </div>
+                            </div>
+                            
+                            <button type="submit" onClick={this.handleSubmit}
+                                className="btn btn-default mt-5 col-8 col-sm-8 col-md-4 col-lg-3 col-xl-3">
+                                Search for Charities
+                            </button>
+                            {this.state.loading && 
+                                    <img src={spinner} className="mt-5" alt="loading..." style={{height: 30, paddingLeft:30}}/>
+                            }
+
+                            <p />
+                            {this.state.doneCharitySearch && this.state.charities.length === 0 &&
+                            this.state.cause !== '' && this.state.location !== '' &&
+                                <h6>
+                                    Sorry, no charities supporting {this.state.cause.value} in {this.state.location.value}.
+                                    <p />
+                                    Please modify your search.
+                                </h6>
+                            }
+                            {this.state.doneCharitySearch && this.state.charities.length === 0 &&
+                            this.state.cause === '' && this.state.location === '' &&
+                                <h6>
+                                    Choose a cause and a location so that we can find the right charities for you!
+                                </h6>
+                            }
+
+                        </div>
+                                        
+                    </form>
+
+                }
+
+                {
+                    this.state.doneCharitySearch && this.state.charities.length > 0 &&
+                    <div className="m-3 px-3">
+
+                        <button type="button" onClick={this.handleClickToSearch}
+                            className="btn btn-default mb-2">
+                            Back to search
+                        </button>
+
+                        <h5 className="my-3">
+                            <strong>{valueCause}</strong> in <strong>{valueLocation}</strong>
+                        </h5>
+                        
+                        <div className="row d-flex align-items-center justify-content-between px-3">
+                            <h6 className="small mb-0">
+                                Showing {(currentPage - 1) * charitiesPerPage + 1} to {Math.min(currentPage * charitiesPerPage, charities.length)} of {charities.length} results
+                            </h6> 
+
+                            <div>
+                                <Pagination
+                                    hideDisabled
+                                    activePage={currentPage}
+                                    itemsCountPerPage={charitiesPerPage}
+                                    totalItemsCount={charities.length}
+                                    pageRangeDisplayed={5}
+                                    onChange={this.handleClickOnPageNumber}
+                                />
                             </div>
                         </div>
                         
-                        <button type="submit" onClick={this.handleSubmit}
-                            className="btn btn-default mt-5 col-8 col-sm-8 col-md-4 col-lg-3 col-xl-3">
-                            Search for Charities
+                        <ul className="row card-group list-unstyled">{renderCharities}</ul>
+                        
+                        <div className="row d-flex justify-content-between px-3">
+                            <h6 className="small mb-0">
+                                Showing {(currentPage - 1) * charitiesPerPage + 1} to {Math.min(currentPage * charitiesPerPage, charities.length)} of {charities.length} results
+                            </h6>
+
+                            <div>
+                                <Pagination
+                                    hideDisabled
+                                    activePage={currentPage}
+                                    itemsCountPerPage={charitiesPerPage}
+                                    totalItemsCount={charities.length}
+                                    pageRangeDisplayed={5}
+                                    onChange={this.handleClickOnPageNumber}
+                                />
+                            </div>
+                        </div>
+
+                        <button type="button" onClick={this.handleClickToSearch}
+                            className="btn btn-default mb-2">
+                            Back to search
                         </button>
-                        {this.state.loading && 
-                                <img src={spinner} className="mt-5" alt="loading..." style={{height: 30, paddingLeft:30}}/>
-                        }
-
-                        <p />
-                        {this.state.doneCharitySearch && this.state.charities.length === 0 &&
-                        this.state.cause !== '' && this.state.location !== '' &&
-                            <h6>
-                                Sorry, no charities supporting {this.state.cause.value} in {this.state.location.value}.
-                                <p />
-                                Please modify your search.
-                            </h6>
-                        }
-                        {this.state.doneCharitySearch && this.state.charities.length === 0 &&
-                        this.state.cause === '' && this.state.location === '' &&
-                            <h6>
-                                Choose a cause and a location so that we can find the right charities for you!
-                            </h6>
-                        }
-
                     </div>
-                                       
-                </form>
+                }
                 
-                <p></p>
-                
-                <div className="mx-3 px-3 my-5">
-
-                    {/* {this.state.doneCharitySearch && this.state.charities.length > 0 &&
-                        <h5 className="mb-3 p-2" style={{color:"#000", background:"rgba(29, 233, 182, 0.85)"}}>
-                            Found {this.state.charities.length} charities for you:
-                        </h5>
-                    }   */ } 
-
-                    <ul className="row card-group list-unstyled">{charityListItems}</ul>
-
-                </div>
-
                 <footer className="page-footer stylish-color-dark font-small">
                     <Container className="py-5">
                     <Row className="text-center d-flex justify-content-center">
