c7d4916c418eb409ef69d333a171d5b634aafdf4
Evie Ma <yfma89@gmail.com>
2018-04-08 12:33:29 +1000

    embedded typeform contact form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,58 +1,24 @@
-import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Input, Fa } from 'mdbreact';
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'mdbreact';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
 
-class Contact extends Component {
+const Contact =  () => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            sendClicked: false
-        };
-        this.handleOnClickToSend = this.handleOnClickToSend.bind(this);
-    }
+    return(
+        <div>
 
-    handleOnClickToSend() {
-        this.setState({
-            sendClicked: true
-        });
-    }
+            <Breadcrumb className="small mb-0">
+                <BreadcrumbItem><a href="/home"><i className="fa fa-home"></i></a></BreadcrumbItem>
+                <BreadcrumbItem active>Contact us</BreadcrumbItem>
+            </Breadcrumb>
 
-    render() {
-        return(
-            <div>
-    
-                <Breadcrumb className="small">
-                    <BreadcrumbItem><a href="/home"><i className="fa fa-home"></i></a></BreadcrumbItem>
-                    <BreadcrumbItem active>Contact us</BreadcrumbItem>
-                </Breadcrumb>
-                
-                <form className="mx-5 py-5">
-                    <p className="h5 text-center my-4">We'd love to hear from you!</p>
-                    <p className="text-center">If you have any questions or concerns, feel free to drop us an email.</p>
-                    
-                    <Input label="Your name" icon="user" group type="email" validate error="wrong" success="right"/>
-                    <Input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
-                    <Input label="Subject" icon="tag" group type="email" validate error="wrong" success="right"/>
-                    <Input type="textarea" label="Your message" icon="pencil"/>
-                
-                    <div className="text-center">
-                        <button className="btn btn-info" type="button" onClick={this.handleOnClickToSend}>
-                            Send 
-                            <Fa icon="paper-plane-o" className="ml-1"/>
-                        </button>
-                        {
-                            this.state.sendClicked &&
-                            <p className="mt-4">
-                                Thank you for contacting us. 
-                                We'll get back to you as soon as we can!
-                            </p>
-                        }
-                    </div>
-                </form>
-    
+            {/* form wrapper */}
+            <div style={{height:"80vh",}} >
+                <ReactTypeformEmbed url={'https://yifei2.typeform.com/to/tkK1rq'} />
             </div>
-        );
-    }
+
+        </div>
+    );
 
 }
 
