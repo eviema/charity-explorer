3cd26ac983d0c35ecb7291612691c14db2523d5d
Evie Ma <yfma89@gmail.com>
2018-05-16 23:38:44 +1000

    changed overlay opacity of landing top

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -454,9 +454,9 @@ class Landing extends Component {
       left: "0", 
       right: "0", 
       bottom: "0", 
-      background: "rgba(0, 0, 0, .5)",
+      background: "rgba(0, 0, 0, .2)",
       // background: "linear-gradient(141deg, #006064, #01579B)",
-      opacity: ".6",
+      // opacity: ".6",
       zIndex: "2", 
     };
 
